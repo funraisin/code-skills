fa3b7a21f9bb507fca8afcbdd5f1601992a3dd27
Christopher Riding <chris@superduper.co.nz>
2021-08-27 16:58:24 +1200

    Update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 (function() {
-	console.log('Welcome!');
+	console.log('Welcome here!');
+
+	const imagesSection = document.querySelector('.imagesSection');
+
+	//jQuery
+	// const imagesSection = $('.imagesSection');
+
+	if(imagesSection) {
+		let html = '';
+
+		for(i = 0; i < 32; i++) {
+			html += `<img src="images/book-${i}.png" alt="Book ${i + 1}" />`;
+		}
+
+		imagesSection.innerHTML = html;
+	}
+
 })();
 
 // JQuery
